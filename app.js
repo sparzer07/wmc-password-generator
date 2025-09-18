f9695ef9986d5e114f33925a26a168dfcb322d17
FHGxFREEZE <sparzer07@gmail.com>
2025-09-18 09:11:56 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function generatePassword(length) {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+  return password;
+}
+
+console.log(generatePassword(12));
+
